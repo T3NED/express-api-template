f872abf5388e7873f54c84f738b852517bce4474
T3NED <software@t3ned.com>
2022-05-24 22:31:35 +0100

    Add integer env resolver

---
+++ src/config/env/resolvers/envInteger.ts
@@ -0,0 +1,11 @@
+import type { EnvResolver } from "../interface";
+import { env } from "./env";
+
+const isValidInteger = (value: string) => Number(value) === parseInt(value);
+
+export const envInteger = ((key: string, required = true, defaultValue?: number): number | undefined => {
+	const value = env(key, required, `${defaultValue}`);
+	if (typeof value === "undefined") return 0;
+	if (!isValidInteger(value)) throw new Error(`Invalid integer environment variable: ${key}`);
+	return parseInt(value);
+}) as EnvResolver<number>;
