bf93f64b9ff1a916aa7c40fd58fb69ea6ef06686
T3NED <software@t3ned.com>
2022-05-24 22:43:17 +0100

    Update exports for app config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,7 @@
 import { env, envInteger, envPossibles } from "./env/resolvers";
 
-export = {
-	name: env("APP_NAME", true),
-	port: envInteger("APP_PORT", false, 3000),
-	env: envPossibles("APP_ENV", ["production", "development", "staging", "local"], "local"),
-};
+const name = env("APP_NAME", true);
+const port = envInteger("APP_PORT", false, 3000);
+const environment = envPossibles("APP_ENV", ["production", "development", "staging", "local"], "local");
+
+export { name, port, environment };
