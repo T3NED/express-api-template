f8694190a9a717a0d23abc5166f549735410ff9e
T3NED <software@t3ned.com>
2022-05-24 22:48:01 +0100

    Set port config option as required

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,7 @@
 import { env, envInteger, envPossibles } from "./env/resolvers";
 
 const name = env("APP_NAME", true);
-const port = envInteger("APP_PORT", false, 3000);
+const port = envInteger("APP_PORT", true, 3000);
 const environment = envPossibles("APP_ENV", ["production", "development", "staging", "local"], "local");
 
 export { name, port, environment };
