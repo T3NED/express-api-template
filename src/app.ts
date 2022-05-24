9270227041c2fe8dd7c3ad82860ff9726ef40d4b
T3NED <software@t3ned.com>
2022-05-24 22:52:18 +0100

    Enable cors and use middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import "./index";
 
-import express from "express";
+import { HttpStatus } from "#constants/http";
 import { port } from "#config/app";
+import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+	cors({
+		origin: "*",
+		optionsSuccessStatus: HttpStatus.Ok,
+		credentials: true,
+	}),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, "0.0.0.0", () => {
 	console.log(`Serving on http://localhost:${port}`);
