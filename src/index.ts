e5a45a162ae7130e3fada7b8c48012447e83e583
T3NED <software@t3ned.com>
2022-05-24 22:37:31 +0100

    Add error handling for uncaught exceptions and unhandled rejections

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 import "dotenv/config";
+
+process.on("uncaughtException", (error) => {
+	// TODO: update to logger once implemented
+	console.error(error);
+});
+
+process.on("unhandledRejection", (error) => {
+	// TODO: update to logger once implemented
+	console.error(error);
+});
+
+process.on("SIGINT", () => {
+	// TODO: update to logger once implemented
+	console.warn("Received SIGINT signal, exiting...");
+	process.exit(0);
+});
