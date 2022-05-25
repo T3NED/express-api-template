8a996f37f2821a3d54ae84f547b5b5196f9a937c
T3NED <software@t3ned.com>
2022-05-25 20:40:56 +0100

    Add options to controller

--- src/lib/controller/Controller.ts
+++ src/lib/controller/Controller.ts
@@ -1,9 +1,23 @@
 import { HttpStatus } from "#constants/http";
 
 export abstract class Controller {
+	public options: ControllerOptions;
+
+	public constructor(options: Partial<ControllerOptions> = {}) {
+		this.options = {
+			baseRoute: options.baseRoute ?? "/",
+			defaultVersion: options.defaultVersion ?? "v1",
+		};
+	}
+
 	protected json(data: ControllerJsonData, status = HttpStatus.Ok): ControllerData {
 		return { status, data };
 	}
+}
+
+export interface ControllerOptions {
+	baseRoute: `/${string}`;
+	defaultVersion: `v${number}`;
 }
 
 export interface ControllerData {
