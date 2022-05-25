ae8b137071acf2c1a196808cf694d87bb1300c9a
T3NED <software@t3ned.com>
2022-05-25 19:08:50 +0100

    Fix typo in config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { env, envInteger } from "#lib/env";
 
 const database = env("POSTGRES_DATABASE", true);
-const username = env("POSTGRES_USERAME", true);
+const username = env("POSTGRES_USERNAME", true);
 const password = env("POSTGRES_PASSWORD", true);
 const host = env("POSTGRES_HOST", true);
 const port = envInteger("POSTGRES_PORT", true);
--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,6 +1,6 @@
 import { env, envInteger } from "#lib/env";
 
-const username = env("REDIS_USERAME", true);
+const username = env("REDIS_USERNAME", true);
 const host = env("REDIS_HOST", true);
 const port = envInteger("REDIS_PORT", true);
 
