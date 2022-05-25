53e8783d929063a5a8ae4d5f60081c7efab45570
T3NED <software@t3ned.com>
2022-05-25 17:54:12 +0100

    Add redis config to redis provider

--- src/providers/redis/RedisProvider.ts
+++ src/providers/redis/RedisProvider.ts
@@ -1,4 +1,5 @@
 import RedisClient, { Redis } from "ioredis";
+import { username, host, port } from "#config/redis";
 import { Logger } from "#utils";
 
 export class RedisProvider {
@@ -8,7 +9,11 @@ export class RedisProvider {
 	public constructor() {
 		if (RedisProvider.#instance) throw new Error("RedisProvider is a singleton");
 
-		const client = new RedisClient(process.env.REDIS_URI as string);
+		const client = new RedisClient({
+			username,
+			host,
+			port,
+		});
 
 		client.on("connect", () => Logger.info("Connected to redis"));
 		client.on("error", (error) => Logger.error(error));
