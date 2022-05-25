9af03807487a7fe208dcf2821fa1d0f0775624a3
T3NED <software@t3ned.com>
2022-05-25 21:56:56 +0100

    Refactor app to use controller loader

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,8 @@
 import "./index";
 
-import { convertError, handleError, unknownRoute } from "#middleware";
-import { HttpStatus } from "#constants/http";
-import { Logger } from "#utils";
-import { port } from "#config/app";
-
+import { ControllerLoader } from "#lib/controller";
 import express from "express";
-import cors from "cors";
-
-import v1 from "./routes/v1";
 
 const app = express();
-
-app.use(cors({ origin: "*", optionsSuccessStatus: HttpStatus.Ok, credentials: true }));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/v1", v1);
-
-app.use(unknownRoute);
-
-app.use(convertError);
-app.use(handleError);
-
-app.listen(port, "0.0.0.0", () => {
-	Logger.info(`Serving on http://localhost:${port}`);
-});
+const loader = new ControllerLoader(app);
+loader.serve();
