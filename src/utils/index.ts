11cac6939a0974e5607306cbc30eed9be9594a57
T3NED <software@t3ned.com>
2022-05-25 18:44:00 +0100

    Add implement utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./logger/logger";
 export * from "./snowflake/Snowflake";
 export * from "./snowflake/ServiceSnowflake";
 export * from "./catchServerError/catchServerError";
+export * from "./implement/implement";
