86fafcc6c3238457be253b384faa754e3ef10d0b
T3NED <software@t3ned.com>
2022-05-25 17:34:53 +0100

    Update config to use app constants

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,8 @@
 import { env, envInteger, envPossibles } from "./env/resolvers";
+import { environments } from "#constants/app";
 
 const name = env("APP_NAME", true);
 const port = envInteger("APP_PORT", true, 3000);
-const environment = envPossibles("APP_ENV", ["production", "development", "staging", "local"], "local");
+const environment = envPossibles("APP_ENV", environments, "local");
 
 export { name, port, environment };
