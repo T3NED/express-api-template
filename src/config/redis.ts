95cb01e49d8d6abafc9d6695262b69ff73a88ca9
T3NED <software@t3ned.com>
2022-05-25 17:54:03 +0100

    Remove database redis config option

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,8 +1,7 @@
 import { env, envInteger } from "./env/resolvers";
 
-const database = env("REDIS_DATABASE");
 const username = env("REDIS_USERAME", true);
 const host = env("REDIS_HOST", true);
 const port = envInteger("REDIS_PORT", true);
 
-export { database, username, host, port };
+export { username, host, port };
