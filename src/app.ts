1dfeb76373fd6b123dfce91207200862fb802bfd
T3NED <software@t3ned.com>
2022-05-25 17:10:40 +0100

    Add middleware and v1 router to app

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,25 @@
 import "./index";
 
 import { HttpStatus } from "#constants/http";
+import { convertError, handleError } from "#middleware";
 import { Logger } from "#utils";
 import { port } from "#config/app";
 import express from "express";
 import cors from "cors";
 
+import v1 from "./routes/v1";
+
 const app = express();
 
-app.use(
-	cors({
-		origin: "*",
-		optionsSuccessStatus: HttpStatus.Ok,
-		credentials: true,
-	}),
-);
+app.use(cors({ origin: "*", optionsSuccessStatus: HttpStatus.Ok, credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1", v1);
+
+app.use(convertError);
+app.use(handleError);
 
 app.listen(port, "0.0.0.0", () => {
 	Logger.info(`Serving on http://localhost:${port}`);
