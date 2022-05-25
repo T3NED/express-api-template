7e15410c9f75a42e4af00c72fce727d658187756
T3NED <software@t3ned.com>
2022-05-25 16:55:03 +0100

    Add convert error middleware

---
+++ src/middleware/errors/convertError.ts
@@ -0,0 +1,10 @@
+import type { Request, Response, NextFunction } from "express";
+import { ApiError, InternalServerError } from "#struct";
+import { Logger } from "#utils";
+
+export const convertError = (error: Error, _req: Request, _res: Response, next: NextFunction): void => {
+	if (error instanceof ApiError) return next(error);
+
+	Logger.error(error);
+	next(InternalServerError());
+};
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,1 +1,2 @@
 export * from "./errors/handleError";
+export * from "./errors/convertError";
