595e3887f70cc7f22419586d1f7de7dc685dea32
T3NED <software@t3ned.com>
2022-05-25 17:29:32 +0100

    Add app constants

---
+++ src/constants/app.ts
@@ -0,0 +1,8 @@
+export enum AppEnv {
+	Production = "production",
+	Development = "development",
+	Staging = "staging",
+	Local = "local",
+}
+
+export const environments = [AppEnv.Production, AppEnv.Development, AppEnv.Staging, AppEnv.Local];
