e194501084d4c0c500e64f219d6044dcbd4a27cc
T3NED <software@t3ned.com>
2022-05-25 19:14:49 +0100

    Make redis username optional

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,7 @@
 import { env, envInteger } from "#lib/env";
 
-const username = env("REDIS_USERNAME", true);
 const host = env("REDIS_HOST", true);
 const port = envInteger("REDIS_PORT", true);
+const username = env("REDIS_USERNAME", false);
 
 export { username, host, port };
