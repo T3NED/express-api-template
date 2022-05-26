187540bfd69df259a5889e1446aa5e9b76b753fb
T3NED <software@t3ned.com>
2022-05-26 21:33:12 +0100

    Update ratelimit for create user

--- src/controllers/user/ratelimits/createUser.ts
+++ src/controllers/user/ratelimits/createUser.ts
@@ -3,4 +3,4 @@ import { RateLimit, ratelimit } from "#struct";
 export const createUser = new RateLimit()
 	.setMajorParameter("users")
 	.setId(ratelimit().users.get)
-	.setBucket(1000, 60_000);
+	.setBucket(3, 60_000);
