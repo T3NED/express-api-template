73f94c44565ee3fea23d502683caf50cecc78ee2
T3NED <software@t3ned.com>
2022-05-26 01:32:59 +0100

    Wrap UsernameAlreadyInUseError in a function

--- src/struct/errors/api/codes/user/UsernameAlreadyInUseError.ts
+++ src/struct/errors/api/codes/user/UsernameAlreadyInUseError.ts
@@ -1,7 +1,9 @@
 import { HttpStatus } from "#constants/http";
 import { ApiError, ApiErrorCode } from "#struct";
 
-export const UsernameAlreadyInUseError = new ApiError()
-	.setStatus(HttpStatus.BadRequest)
-	.setCode(ApiErrorCode.UsernameInUse)
-	.setMessage("Username is already in use.");
+export const UsernameAlreadyInUseError = () => {
+	return new ApiError()
+		.setStatus(HttpStatus.BadRequest)
+		.setCode(ApiErrorCode.UsernameInUse)
+		.setMessage("Username is already in use.");
+};
