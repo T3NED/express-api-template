1ee07160622a946f080ddc200afd3a338aea87c8
T3NED <software@t3ned.com>
2022-05-29 00:02:45 +0100

    Add constant to check which environment is in use

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,8 +1,17 @@
+import { environment } from "#config/app";
+
 export enum AppEnv {
 	Production = "production",
 	Development = "development",
 	Staging = "staging",
 	Local = "local",
 }
+
+export const is = {
+	production: AppEnv.Production === environment,
+	development: AppEnv.Development === environment,
+	staging: AppEnv.Staging === environment,
+	local: AppEnv.Local === environment,
+};
 
 export const environments = [AppEnv.Production, AppEnv.Development, AppEnv.Staging, AppEnv.Local];
