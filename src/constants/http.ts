bb7f7e4f083039cb28a5fe0b79a495af36884235
T3NED <software@t3ned.com>
2022-05-29 04:13:02 +0100

    Update http status codes

--- src/constants/http.ts
+++ src/constants/http.ts
@@ -2,19 +2,25 @@ export enum HttpStatus {
 	// Success
 	Ok = 200,
 	Created,
+	Accepted,
 	NoContent = 204,
+	ResetContent,
 	PartialContent,
 
 	// Redirectional
-	Found = 302,
+	MovedPermanently = 301,
+	Found,
+	NotModified = 304,
+	TemporaryRedirect = 307,
+	PermanentRedirect,
 
 	// Client errors
 	BadRequest = 400,
 	Unauthorized,
-	Forbidden = 403,
+	PaymentRequired,
+	Forbidden,
 	NotFound,
 	MethodNotAllowed,
-	NotAcceptable,
 	Conflict = 409,
 	UnsupportedMediaType = 415,
 	TooManyRequests = 429,
@@ -23,7 +29,6 @@ export enum HttpStatus {
 	InternalServerError = 500,
 	BadGateway = 502,
 	ServiceUnavailable,
-	GatewayTimeout,
 }
 
 export enum HttpHeader {
