8fadb088748dae59175b2663017f94a565fe13e0
T3NED <software@t3ned.com>
2022-05-29 04:37:10 +0100

    Add api error codes for all common http errors

--- src/struct/errors/api/ApiErrorCode.ts
+++ src/struct/errors/api/ApiErrorCode.ts
@@ -1,9 +1,17 @@
 export enum ApiErrorCode {
-	// Common errors
+	// Common
 	BadRequest = 0,
 	Unauthorized = 0,
+	PaymentRequired = 0,
+	Forbidden = 0,
+	NotFound = 0,
+	MethodNotAllowed = 0,
+	Conflict = 0,
+	UnsupportedMediaType = 0,
 	TooManyRequests = 0,
 	InternalServerError = 0,
+	BadGateway = 0,
+	ServiceUnavailable = 0,
 
 	// Unknown resources
 	UnknownRoute = 40_000,
