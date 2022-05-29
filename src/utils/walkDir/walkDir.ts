df4944d896c7d656ff9f6bbac4355bfae281142f
T3NED <software@t3ned.com>
2022-05-29 03:33:04 +0100

    Update walkDir to sync from async

--- src/utils/walkDir/walkDir.ts
+++ src/utils/walkDir/walkDir.ts
@@ -1,7 +1,7 @@
-import { readdir } from "fs/promises";
+import { readdirSync } from "fs";
 
-export async function walkDir(path: string, paths: string[] = []): Promise<string[]> {
-	for (const file of await readdir(path, { withFileTypes: true })) {
+export function walkDir(path: string, paths: string[] = []): string[] {
+	for (const file of readdirSync(path, { withFileTypes: true })) {
 		if (file.isDirectory()) walkDir(`${path}/${file.name}`, paths);
 		else if (file.name.endsWith(".js")) paths.push(`${path}/${file.name}`);
 	}
