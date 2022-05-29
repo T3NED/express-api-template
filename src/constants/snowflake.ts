1c8fd5070d4d155c03a041462af2cf9ee3f45b11
T3NED <software@t3ned.com>
2022-05-29 04:53:53 +0100

    Update snowflake epoch

--- src/constants/snowflake.ts
+++ src/constants/snowflake.ts
@@ -1,4 +1,4 @@
-// 2021-01-01 at 00:00:00.000 UTC+0
-export const serviceEpoch = 1609459200000n;
+// 2022-01-01 at 00:00:00.000 UTC+0
+export const serviceEpoch = 1640995200000n;
 
 export const regex = /^\d{16,20}$/;
